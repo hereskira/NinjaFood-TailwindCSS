d93264c56925b3d559544b3d40a23eeeb532ba21
hereskira <shakiraregalado@gmail.com>
2024-06-19 17:46:47 +0800

    lesson-5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.{html,js}"],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}', // Adjust the paths based on your project structure
+    './public/index.html',],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      }
+    },
   },
   plugins: [],
 }
